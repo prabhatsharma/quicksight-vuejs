57ca296eb2105ea1c0673fa4f883ab9d7344e29d
Prabhat Sharma <hi.prabhat@gmail.com>
2021-03-10 16:28:03 -0800

    branding

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/quicksight">Quicksight</router-link>
+    <router-link to="/quicksight">QuickSight</router-link>
   </div>
   <router-view/>
 </template>
